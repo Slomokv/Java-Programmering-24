4330aa994247794d7b253794df6bdc1ccd04c110
Daniel <daslomo@gmail.com>
2024-09-27 20:59:29 +0200

    Class task additions, task 3

---
+++ exercises/standard_exercises/Disc.java
@@ -0,0 +1,19 @@
+package exercises.standard_exercises;
+
+public class Disc {
+
+    String name;
+    int speed;
+    double weight;
+
+    
+
+
+} 
+
+
+
+
+// Disc destroyer = new Disc("Destroyer", 12, 167.1);
+
+// Disc teeBird = new Disc ("Teebird", 7, 173.2);
